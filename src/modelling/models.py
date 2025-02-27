3d4defb933e46e312647ab37dc39172920d13f70
lmeller-git <louis.meller@icloud.com>
2025-02-27 11:05:01 +0100

    [fix] updated dropout parameter in block function and modified output layers in models (generated) original: small fixes to models

--- src/modelling/models.py
+++ src/modelling/models.py
@@ -2,12 +2,14 @@ from torch import nn
 import torch
 
 
-def block(in_shape: int, out: float, act: nn.Module = nn.ReLU) -> nn.Module:
+def block(
+    in_shape: int, out: float, act: nn.Module = nn.ReLU, drp: float = 0.42
+) -> nn.Module:
     layer = nn.Sequential(
         nn.Linear(in_shape, out, bias=False),
         act(),
         nn.BatchNorm1d(out),
-        nn.Dropout(0.42),
+        nn.Dropout(drp),
     )
 
     return layer
@@ -18,7 +20,7 @@ class BasicMLP(nn.Module):
         super().__init__()
         self.input = block(in_shape, hidden_dim)
         self.hidden = block(hidden_dim, hidden_dim)
-        self.out = block(hidden_dim, 1, act=nn.Identity)
+        self.out = nn.Linear(hidden_dim, 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.input(x)
@@ -31,9 +33,10 @@ class MLP(nn.Module):
         super().__init__()
         self.input = block(in_shape, hidden_dim[0])
         self.hidden = nn.Sequential(
-            block(hidden_dim[0], hidden_dim[1]), block(hidden_dim[1], hidden_dim[2])
+            block(hidden_dim[0], hidden_dim[1]),
+            block(hidden_dim[1], hidden_dim[2], drp=0.2),
         )
-        self.out = block(hidden_dim[2], 1, act=nn.Identity)
+        self.out = nn.Linear(hidden_dim[2], 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.input(x)
@@ -47,9 +50,9 @@ class LeakyMLP(nn.Module):
         self.input = block(in_shape, hidden_dim[0])
         self.hidden = nn.Sequential(
             block(hidden_dim[0], hidden_dim[1], act=nn.LeakyReLU),
-            block(hidden_dim[1], hidden_dim[2], act=nn.LeakyReLU),
+            block(hidden_dim[1], hidden_dim[2], act=nn.LeakyReLU, drp=0.2),
         )
-        self.out = block(hidden_dim[2], 1, act=nn.Identity)
+        self.out = nn.Linear(hidden_dim[2], 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.input(x)
@@ -65,7 +68,7 @@ class Siamese(nn.Module):
             self.shared_layers.append(block(hidden_dim, hidden_dim))
         self.output_dim = hidden_dim * 3
         self.head = nn.Sequential(
-            block(self.output_dim, hidden_dim), nn.Linear(hidden_dim, 1)
+            block(self.output_dim, hidden_dim, drp=0.2), nn.Linear(hidden_dim, 1)
         )
 
     def forward_single(self, x):
