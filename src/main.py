4ace81f2f8eac513c282e42a76fd13053b423d9a
lmeller-git <lmeller@ethz.ch>
2025-02-27 12:02:42 +0100

    [feature] added Siamese model and updated hidden dimensions in main.py (generated) original: stuff

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from src import train, TrainParams, BasicMLP, MLP, LeakyMLP
+from src import train, TrainParams, BasicMLP, MLP, LeakyMLP, Siamese
 from argparse import ArgumentParser
 
 
 def main(args):
     print(args)
-    model = LeakyMLP(768)
+    model = LeakyMLP(hidden_dim = (786, 512, 256))#Siamese(hidden_dim=512, n_layers=2) #LeakyMLP(768)
     if args.mode == "cv":
         params = TrainParams(
             args.data,
--- src/modelling/__init__.py
+++ src/modelling/__init__.py
@@ -1,5 +1,5 @@
-from src.modelling.models import BasicMLP, MLP, LeakyMLP
+from src.modelling.models import BasicMLP, MLP, LeakyMLP, Siamese
 from src.modelling.training import TrainParams, train
 from src.modelling.eval import LossPlotter
 
-__all__ = ["BasicMLP", "TrainParams", "train", "LossPlotter", "MLP", "LeakyMLP"]
+__all__ = ["BasicMLP", "TrainParams", "train", "LossPlotter", "MLP", "LeakyMLP", "Siamese"]
