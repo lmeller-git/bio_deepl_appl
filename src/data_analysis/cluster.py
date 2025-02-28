ef835604c88dc5872954496048cbeeb9d0cd7a82
tboesch-git <takumi.boesch@web.de>
2025-02-28 11:34:09 +0100

    adsfasfd

--- src/data_analysis/cluster.py
+++ src/data_analysis/cluster.py
@@ -6,23 +6,23 @@ from src.utils import Plotter
 
 class Clusterplotter(Plotter):
     def __init__(self):
-        self.df = None
+        self.df_pivot = None
 
     def plot(self) -> None:
-        sns.clustermap(self.df.pivot(columns="mutation", values="ddG_ML").T, figsize=(10, 5), cmap="viridis", annot=True)
+        sns.clustermap(df_pivot, figsize=(10, 5), cmap='viridis', annot=True)        
         plt.show()
 
     def update(self, csv: str) -> None:
-        self.df = pd.read_csv(csv)
-        self.df = df[["mut_type", "ddG_ML"]]
-        self.df[["mut_from", "mut_to"]] = df["mut_type"].str.extract(r"([A-Z])\d*([A-Z])")
-        self.df["mutation"] = df["mut_from"] + df["mut_to"]
-        self.df.drop(columns=["mut_from", "mut_to"], inplace=True)
-        self.df.dropna(subset=["mutation"], inplace=True)
-        
+        df = pd.read_csv(csv)
+        df = df[["mut_type", "ddG_ML"]]
+        df[["mut_from", "mut_to"]] = df["mut_type"].str.extract(r"([A-Z])\d*([A-Z])")
+        df["mutation"] = df["mut_from"] + df["mut_to"]
+        df.drop(columns=["mut_from", "mut_to"], inplace=True)
+        df.dropna(subset=["mutation"], inplace=True)
+        df_pivot = df.pivot(index='mutation', columns='mutation', values='ddG_ML')
 
     def clear(self):
-        self.df = None
+        self.df_pivot = None
 
 
 def cluster_plot(p: str = ".data/project_data/mega_train.csv"):
