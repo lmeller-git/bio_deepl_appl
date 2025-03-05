0f48688b37e3275b6f62439c59831f103f332630
lmeller-git <louis.meller@icloud.com>
2025-03-05 14:38:31 +0100

    [fix] corrected the data path in plot_mut_dist function call in content.py (generated) original: de

--- src/utils/emb/content.py
+++ src/utils/emb/content.py
@@ -381,7 +381,9 @@ def cross_validate(
     map_plotter.should_save("qual_map" + p.split("/")[-1])
     map_plotter.plot()
 
-    plot_mut_dist(data=OUT, which=selected_groups, name=p.split("/")[-1])
+    plot_mut_dist(
+        data=p.split("/")[:-2] + "/", which=selected_groups, name=p.split("/")[-1]
+    )
 
     return
 
